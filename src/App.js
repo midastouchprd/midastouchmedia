d79c51521c52b3ea91ac556a7578195231bb6652
Tosin Awofeso <midastouchproductions@gmail.com>
2018-01-23 05:52:47 -0600

    cloudfront, and mp4 vids and such

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import './bkg-vid.css';
+
+// d3q9x42dh2gz6c.cloudfront.net/midastouchmedia-bkg.mp4
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="fullscreen-bg">
+          <video loop muted autoPlay className="fullscreen-bg__video">
+            <source
+              src="https://d3q9x42dh2gz6c.cloudfront.net/midastouchmedia-bkg.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
         <a>Midas Touch Media</a>
       </div>
     );
