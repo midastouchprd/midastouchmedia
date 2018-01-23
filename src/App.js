9534a453bc53e87cb1e349e222e87b823a7d8020
Tosin Awofeso <midastouchproductions@gmail.com>
2018-01-23 05:02:28 -0600

    styling for main jumbotron

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>midastouchmedia.com</h1>
+        <a>Midas Touch Media</a>
       </div>
     );
   }
